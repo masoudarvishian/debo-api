7bbaa0dc188fc8c7b3155cebe71d8c0a4e49a78d
Masoud Darvishian <masoudarvishian@gmail.com>
2019-10-25 12:06:51 +0330

    Working on push notification...

--- src/UI/DEBO.API/Hubs/NotifyHub.cs
+++ src/UI/DEBO.API/Hubs/NotifyHub.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System.Threading.Tasks;
 
 namespace DEBO.API.Hubs
 {
     public class NotifyHub : Hub<ITypedHubClient>
     {
-        public Task SendMessageToAll(string message)
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
